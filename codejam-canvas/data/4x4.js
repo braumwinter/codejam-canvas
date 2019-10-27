5e0f0f61df99b92147fb66f083201f748989d51e
Kate <braumwinter@gmail.com>
2019-10-27 22:02:49 +0300

    feat: add layout for canvas

---
+++ codejam-canvas/data/4x4.js
@@ -0,0 +1,26 @@
+const little_img = [
+    [
+        "00BCD4",
+        "FFEB3B",
+        "FFEB3B",
+        "00BCD4"
+    ],
+    [
+        "FFEB3B",
+        "FFC107",
+        "FFC107",
+        "FFEB3B"
+    ],
+    [
+        "FFEB3B",
+        "FFC107",
+        "FFC107",
+        "FFEB3B"
+    ],
+    [
+        "00BCD4",
+        "FFEB3B",
+        "FFEB3B",
+        "00BCD4"
+    ]
+];
